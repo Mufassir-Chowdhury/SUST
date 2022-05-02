c1e57f0a64366a7fb01329536ce032df1f0c2e93
2019331013 <asanulhaquekiron@gmail.com>
2022-05-02 06:40:50 +0600

    adding rounded panel

---
+++ Project/src/components/RoundedPanel.java
@@ -0,0 +1,72 @@
+package Components;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel
+    {
+        private Color backgroundColor;
+        private int cornerRadius = 15;
+        private String text;
+        private Icon icon;
+        public RoundedPanel(LayoutManager layout, int radius) {
+            super(layout);
+            cornerRadius = radius;
+        }
+        public RoundedPanel(LayoutManager layout, int radius, Color bgColor) {
+            super(layout);
+            cornerRadius = radius;
+            backgroundColor = bgColor;
+        }
+
+        public RoundedPanel(int radius) {
+            super();
+            cornerRadius = radius;
+
+        }
+        
+        public RoundedPanel(int radius, String text, Icon icon, Color bgC) {
+            super();
+            cornerRadius = radius;
+            this.text = text;
+            this.icon = icon;
+            backgroundColor = bgC;
+        }
+
+        public RoundedPanel(int radius, String text, Icon icon) {
+            super();
+            cornerRadius = radius;
+            this.text = text;
+            this.icon = icon;
+        }
+
+        public RoundedPanel(int radius, Color bgColor) {
+            super();
+            cornerRadius = radius;
+            backgroundColor = bgColor;
+        }
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Dimension arcs = new Dimension(cornerRadius, cornerRadius);
+            int width = getWidth();
+            int height = getHeight();
+            Graphics2D graphics = (Graphics2D) g;
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            //Draws the rounded panel with borders.
+            if (backgroundColor != null) {
+                graphics.setColor(backgroundColor);
+            } else {
+                graphics.setColor(getBackground());
+            }
+            graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint background
+            graphics.setColor(getForeground());
+//            graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint border
+//             
+        }
+    }
