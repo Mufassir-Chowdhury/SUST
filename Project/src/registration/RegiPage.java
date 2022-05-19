fea5f7f40724ad31c52edf8b02a7a8bc0ded83b7
2019331013 <asanulhaquekiron@gmail.com>
2022-05-20 04:27:40 +0600

    preparing register page

--- Project/src/registration/RegiPage.java
+++ Project/src/registration/RegiPage.java
@@ -7,12 +7,16 @@ import Components.InputFields.TextField;
 import Components.InputFields.TextField.CustomTextField;
 import Components.InputFields.TextField.TYPE;
 import Constants.Sizes;
-import java.awt.FlowLayout;
+import java.awt.*;
+import Main.Main;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import Constants.Fonts;
+import Constants.Colors;
 
 public class RegiPage extends JScrollPane {
 	private JPanel titlePanel, allFieldPanel,
@@ -23,7 +27,7 @@ public class RegiPage extends JScrollPane {
 			row5, hometownPanel, birthdayPanel,
 			row6, bloodGroupPanel, photoPanel,
 			row7, passwordPanel, confirmPasswordPanel;
-						
+
 	private JLabel registerTitle,
 			emailLabel, secondaryEmailLabel,
 			firstNameLabel, lastNameLabel,
@@ -32,7 +36,7 @@ public class RegiPage extends JScrollPane {
 			hometownLabel, birthdayLabel,
 			bloodGroupLabel, photoLabel,
 			passwordLabel, confirmPasswordLabel;
-	
+		
 	private JTextField 
 			emailField, secondaryEmailField,
 			firstNameField, lastNameField,
@@ -41,21 +45,21 @@ public class RegiPage extends JScrollPane {
 			hometownField, birthdayField,
 			bloodGroupField, photoField,
 			passwordField, confirmPasswordField;
-						
-	
-					
 	private JPanel createColumn(JPanel col, JLabel label,
 			String text, JTextField field) 
 	{
 		col = new JPanel();
+		col.setOpaque(false);
 		col.setLayout(new BoxLayout(col, BoxLayout.X_AXIS));
-		label = new JLabel(text);
+		label = new JLabel(text+" :");
+		label.setFont(Fonts.BODY_LARGE);
+        label.setForeground(Colors.ACCENT);
 		field = new TextField("", TYPE.PLAIN);
 		col.add(label);
 		col.add(Box.createHorizontalStrut(Sizes.CARD_SPACING_LONG));
 		col.add(field);
 		return col;
-	}						
+	}
 
 	private void createRow(JPanel row, JPanel col1, JPanel col2,
 			JLabel label1, JLabel label2,
@@ -63,64 +67,75 @@ public class RegiPage extends JScrollPane {
 			JTextField field1, JTextField field2) 
 	{
 		row = new JPanel();
+		row.setOpaque(false);
 		row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
 
 		row.add(createColumn(col1, label1, text1, field1));
 		row.add(Box.createHorizontalStrut(Sizes.CARD_SPACING_LONG));
 		row.add(createColumn(col2, label2, text2, field2));
-		
+
 		allFieldPanel.add(row);
 		allFieldPanel.add(Box.createVerticalStrut(Sizes.CARD_SPACING));
 	}
-	
 
-	public RegiPage() {
-		JScrollPane scrollPane = new JScrollPane();
-		add(scrollPane);
-		setSize(1000, 400);
+	public RegiPage(Main main) {
+			setSize(Sizes.DEFAULT_WINDOW_SIZE);
+			setOpaque(false);
 
-		titlePanel = new JPanel();
-		titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			titlePanel = new JPanel();
+			titlePanel.setOpaque(false);
+			titlePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 
-		registerTitle = new JLabel("Registration");
-		titlePanel.add(registerTitle);
-		setColumnHeaderView(titlePanel);
-		
+			registerTitle = new JLabel("Registration");
+			registerTitle.setFont(Fonts.BODY_LARGE);
+			registerTitle.setForeground(Colors.ACCENT);
+			titlePanel.add(registerTitle);
+			setColumnHeaderView(titlePanel);
+			getColumnHeader().setOpaque(false);
 
-		allFieldPanel = new JPanel();
-		setViewportView(allFieldPanel);
-		allFieldPanel.setLayout(new BoxLayout(allFieldPanel, BoxLayout.Y_AXIS));
-		allFieldPanel.add(Box.createVerticalStrut(Sizes.CARD_SPACING));
+			allFieldPanel = new JPanel();
+			allFieldPanel.setOpaque(false);
+			setViewportView(allFieldPanel);
+			getViewport().setOpaque(false);
 
-		createRow(row1, emailPanel, secondaryEmailPanel,
-				emailLabel, secondaryEmailLabel,
-				"Email", "Secondary Email",
-				emailField, secondaryEmailField);
+			allFieldPanel.setLayout(new BoxLayout(allFieldPanel, BoxLayout.Y_AXIS));
+			allFieldPanel.add(Box.createVerticalStrut(Sizes.CARD_SPACING));
 
-		createRow(row2, firstNamePanel, lastNamePanel,
-				firstNameLabel, lastNameLabel,
-				"First Name", "Last Name",
-				firstNameField, lastNameField);
-		
-		createRow(row3, registrationNoPanel, departmentPanel,
-				registrationNoLabel, departmentLabel,
-				"Registration No", "Department",
-				registrationNoField, departmentField);
-		
-		createRow(row4, sessionPanel, contactNoPanel,
-				sessionLabel, contactNoLabel,
-				"Session", "Contact No",
-				sessionField, contactNoField);
+			createRow(row1, emailPanel, secondaryEmailPanel,
+					emailLabel, secondaryEmailLabel,
+					"Email", "Secondary Email",
+					emailField, secondaryEmailField);
 
-		createRow(row5, hometownPanel, birthdayPanel,
-				hometownLabel, birthdayLabel,
-				"Hometown", "Birthday",
-				hometownField, birthdayField);
+			createRow(row2, firstNamePanel, lastNamePanel,
+					firstNameLabel, lastNameLabel,
+					"First Name", "Last Name",
+					firstNameField, lastNameField);
 
-		createRow(row6, bloodGroupPanel, photoPanel,
-				bloodGroupLabel, photoLabel,
-				"Blood Group", "Upload Photo",
-				bloodGroupField, photoField);
+			createRow(row3, registrationNoPanel, departmentPanel,
+					registrationNoLabel, departmentLabel,
+					"Registration No", "Department",
+					registrationNoField, departmentField);
+
+			createRow(row4, sessionPanel, contactNoPanel,
+					sessionLabel, contactNoLabel,
+					"Session", "Contact No",
+					sessionField, contactNoField);
+
+			createRow(row5, hometownPanel, birthdayPanel,
+					hometownLabel, birthdayLabel,
+					"Hometown", "Birthday",
+					hometownField, birthdayField);
+
+			createRow(row6, bloodGroupPanel, photoPanel,
+					bloodGroupLabel, photoLabel,
+					"Blood Group", "Upload Photo",
+					bloodGroupField, photoField);
+
+			createRow(row7, passwordPanel, confirmPasswordPanel,
+					passwordLabel, confirmPasswordLabel,
+					"Password", "Confirm Password",
+					passwordField, confirmPasswordField);
+			
+		}
+
 	}
-
-}
