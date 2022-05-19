68b0241ffa6ec7d45922ea4008e57ff103a64f00
2019331013 <asanulhaquekiron@gmail.com>
2022-05-20 04:19:23 +0600

    bufferedImage

--- Project/src/components/Tools.java
+++ Project/src/components/Tools.java
@@ -1,20 +1,40 @@
 package Components;
 
 import java.awt.Image;
-
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 import java.awt.Dimension;
 
 public class Tools{
 
-    public static ImageIcon imageScale(String source, Dimension dimension){
+    public static ImageIcon imageScaleB(String source, Dimension dimension){
         ImageIcon bgIcon = new ImageIcon(ClassLoader.getSystemResource(source));
 	    Image bgImage = bgIcon.getImage().getScaledInstance(dimension.width, dimension.height, Image.SCALE_SMOOTH);
 	    return new ImageIcon(bgImage);
     }
 
-    
+    public static ImageIcon imageScale(String source, Dimension dimension)
+    {
+        try{
+            BufferedImage original = ImageIO.read(ClassLoader.getSystemResource(source));
+            BufferedImage resized = new BufferedImage(dimension.width, dimension.height, original.getType());
+            Graphics2D g = resized.createGraphics();
+            g.drawImage(original, 0, 0, dimension.width, dimension.height, null);
+            g.dispose();
+            return new ImageIcon(resized);
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+            return new ImageIcon(source);
+            // have to return something so added this.
+        }
+        
+    }
     
     
 }
