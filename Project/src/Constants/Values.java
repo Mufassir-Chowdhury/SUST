a1e750196e37e4cd667f70e85e34c8b8bafa9fba
2019331013 <asanulhaquekiron@gmail.com>
2022-05-24 06:23:31 +0600

    added some contanstants

--- Project/src/Constants/Values.java
+++ Project/src/Constants/Values.java
@@ -4,6 +4,29 @@ public class Values {
     public static final char[] DEFAULT_PASSWORD = new char[]{' ', 'P','a','s','s','w','o','r','d'};
     public static final String PASSWORD_PLACEHOLDER = " Password";
 
+    public static final String EMAIL_PLACEHOLDER = " Email Address ( Academic )";
+
     public static final char PASSWORD_ECHO_CHAR = '\u25cf';
     public static final char PASSWORD_PLAIN_ECHO_CHAR = '\u0000';
+
+    public static final String STUDENT_EMAIL_DOMAIN = "student.sust.edu";
+    public static final String OFFICIALS_EMAIL_DOMAIN = "sust.edu";
+
+    public static final String ONLY_NUMBERS = "[0-9]+";
+    public static final String ONLY_LOWERCASE_LETTERS = "[a-z]+";
+    public static final String AT_LEAST_ONE_NUMBER = "\\.*[0-9]\\.*";
+    public static final String AT_LEAST_ONE_UPPERCASE_LETTER = "\\.*[A-Z]\\.*";
+    
+
+    public static final String WRONG_EMAIL = "Wrong Email";
+    public static final String WRONG_PASSWORD = "Wrong Password";
+
+    public static final String PASSED = "Passed";
+
+    public static final String SHORT_PASSWORD = "short password";
+    public static final String MISSING_CHARACTERS = "some characters are missing";
+
+    
+    
+
 }
