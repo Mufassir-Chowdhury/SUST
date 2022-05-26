582e7ba8c398a079d90dd0583c456624e59a6b0b
Mufassir-Chowdhury <mac22214u@gmail.com>
2022-05-26 12:32:44 +0600

    combined exam and assignment to single EvaluationItem super class

--- Project/src/components/pageView/EvaluationItem.java
+++ Project/src/components/pageView/EvaluationItem.java
@@ -13,32 +13,7 @@ import java.awt.CardLayout;
 import java.awt.Component;
 
 public class EvaluationItem extends Box {
-    public EvaluationItem(Datapoints.Courses course, Datapoints.Courses.Assignment evaluationMethod, Title title, JPanel assignmentPanel) {
-        super(BoxLayout.X_AXIS);
-        setAlignmentX(Component.LEFT_ALIGNMENT);
-        Box line = new Line(new ListItem(
-            evaluationMethod.title, 
-            course.name, 
-            evaluationMethod.date, 
-            String.format("%03d", evaluationMethod.totalMarks) + "   " + String.format("%03d", evaluationMethod.marksObtained)));
-        line.addMouseListener(new MouseAdapter(){
-            @Override
-            public void mouseClicked(MouseEvent e){
-                title.setText("Assignment > " + evaluationMethod.title);
-                Post individualExam = new Post(
-                    evaluationMethod.title,
-                    "Due Date: " + evaluationMethod.date, 
-                    "Total Marks: " + evaluationMethod.totalMarks, 
-                    evaluationMethod.description);
-                assignmentPanel.add(individualExam, evaluationMethod.title);
-                CardLayout cl = (CardLayout)(assignmentPanel.getLayout());
-                cl.show(assignmentPanel, evaluationMethod.title);
-            }
-            
-        });
-        add(line);
-    }
-    public EvaluationItem(Datapoints.Courses course, Datapoints.Courses.Exam evaluationMethod, Title title, JPanel assignmentPanel) {
+    public EvaluationItem(Datapoints.Courses course, Datapoints.Courses.EvaluationItem evaluationMethod, Title title, JPanel assignmentPanel) {
         super(BoxLayout.X_AXIS);
         setAlignmentX(Component.LEFT_ALIGNMENT);
         Box line = new Line(new ListItem(
@@ -59,7 +34,6 @@ public class EvaluationItem extends Box {
                 CardLayout cl = (CardLayout)(assignmentPanel.getLayout());
                 cl.show(assignmentPanel, evaluationMethod.title);
             }
-            
         });
         add(line);
     }
