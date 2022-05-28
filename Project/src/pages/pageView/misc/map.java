9f0d7f210392b33dad46a790d2df5f05654e1f1c
2019331013 <asanulhaquekiron@gmail.com>
2022-05-28 10:43:26 +0600

    trying to draw map

--- Project/src/pages/pageView/misc/map.java
+++ Project/src/pages/pageView/misc/map.java
@@ -1,9 +1,29 @@
 package pages.pageView.misc;
 
+import Components.Label;
+import Components.pageView.Panels.GridBagPanel;
 import Components.pageView.Panels.ViewPort;
+import java.awt.GridBagConstraints;
+import javax.swing.*;
+import javax.swing.JPanel;
+import java.awt.Component;
+import java.awt.Canvas;
+import java.awt.*;
 
 public class map extends ViewPort {
-    public map(){
+    public map() {
         super("Map", null);
+
+        JPanel drawingPanel = new JPanel();
+        drawingPanel.setOpaque(false);
+        drawingPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        Canvas canvas = new Canvas();
+        canvas.setSize(400, 400);
+        // canvas.drawColor(Color.argb(0, 0, 0, 0));
+        
+        // canvas.drawColor(Color.TRANSLUCENT, PorterDuff.Mode.CLEAR);
+        drawingPanel.add(canvas);
+        add(drawingPanel);
+        System.out.println(drawingPanel.getHeight());
     }
 }
