d2f8ffad340081fd11dcaff3c4c8deafe1f93bba
Mufassir-Chowdhury <mac22214u@gmail.com>
2022-08-05 20:41:47 +0600

    updated time to fetch local time

--- Project/src/components/pageView/CurrentTime.java
+++ Project/src/components/pageView/CurrentTime.java
@@ -6,14 +6,21 @@ import Components.Label;
 import Components.pageView.Panels.ListPanel;
 import Constants.Fonts;
 import java.awt.Component;
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CurrentTime extends ListPanel {
     public CurrentTime(){
         add(new Label("Current Time", Fonts.SUBTITLE, Component.LEFT_ALIGNMENT));
         add(Box.createVerticalStrut(-10));
-        add(new Label("12:59 PM", Fonts.SECOND_DISPLAY, Component.LEFT_ALIGNMENT));
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("hh:mm a");
+        LocalTime localTime = LocalTime.now();
+        add(new Label(time.format(localTime), Fonts.SECOND_DISPLAY, Component.LEFT_ALIGNMENT));
         add(Box.createVerticalStrut(-10));
-        add(new Label("Saturday,     April 16, 2022", Fonts.BODY, Component.LEFT_ALIGNMENT));
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("EEEE,         MMMM dd, u");
+        LocalDate localDate = LocalDate.now();
+        add(new Label(date.format(localDate), Fonts.BODY, Component.LEFT_ALIGNMENT));
         add(Box.createVerticalGlue());
     }    
 }
